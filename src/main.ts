5aa7f5bde60d765b0da9ccf0f70ec3132e5dd3cd
kasa021 <mkafa.k11@gmail.com>
2023-10-15 01:46:48 +0900

    refactor:atcoderのapiを分けた

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,10 @@
 import dotenv from "dotenv";
-import axios from "axios";
 import { GatewayIntentBits, Client, Partials } from "discord.js";
 import { AtCoderContestInfo } from "./interface";
+import * as AtCoderAPI from "./atcoderAPI";
 dotenv.config();
 
-const ATCODER_BASE_URL = "https://atcoder.jp/users";
-
-// AtCoderContestInfoのオブジェクト型の配列
 const contestInfo: AtCoderContestInfo[] = [];
-
-const atcoderAPI = axios.create({
-  baseURL: ATCODER_BASE_URL,
-});
-
-const fetchAtCoderContestInfo = async (
-  username: string
-): Promise<AtCoderContestInfo> => {
-  try {
-    const res = await atcoderAPI.get(`/${username}/history/json`);
-    return {
-      username: username,
-      contestResult: res.data,
-    };
-  } catch (error) {
-    console.error("Error fetching AtCoder contest info:", error);
-    throw error;
-  }
-};
-
-const addContestInfo = async (username: string): Promise<string> => {
-  try {
-    const info = await fetchAtCoderContestInfo(username);
-    if (info.contestResult.length === 0) {
-      return `${username}のコンテスト情報が存在しませんでした`;
-    }
-    const index = contestInfo.findIndex(
-      (item) => item.username === info.username
-    );
-    if (index !== -1) {
-      contestInfo[index] = info;
-    } else {
-      contestInfo.unshift(info);
-    }
-    return `${username}の情報を追加しました`;
-  } catch (error) {
-    console.error("Error adding contest info:", error);
-    return "エラーが発生しました";
-  }
-};
-
-const deleteContestInfo = (username: string) => {
-  const index = contestInfo.findIndex((item) => item.username === username);
-  if (index !== -1) {
-    contestInfo.splice(index, 1);
-    return `${username}の情報を削除しました`;
-  } else {
-    return `${username}は登録されていませんでした`;
-  }
-};
-
-const getContestInfo = (username: string) => {
-  const index = contestInfo.findIndex((item) => item.username === username);
-  if (index !== -1) {
-    return contestInfo[index];
-  } else {
-    return null;
-  }
-};
 
 const client = new Client({
   intents: [
@@ -79,10 +17,6 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel],
 });
 
-client.on("ready", async () => {
-  console.log("ready");
-});
-
 // addコマンド: ユーザーのAtCoderの情報を追加する
 client.on("messageCreate", async (message) => {
   if (message.content.startsWith("!add")) {
@@ -91,7 +25,7 @@ client.on("messageCreate", async (message) => {
       message.channel.send("ユーザー名を入力してください");
       return;
     }
-    const response = await addContestInfo(username);
+    const response = await AtCoderAPI.addContestInfo(username, contestInfo);
     message.channel.send(response);
   }
 });
@@ -104,7 +38,7 @@ client.on("messageCreate", (message) => {
       message.channel.send("ユーザー名を入力してください");
       return;
     }
-    const response = deleteContestInfo(username);
+    const response = AtCoderAPI.deleteContestInfo(username, contestInfo);
     message.channel.send(response);
   }
 });
@@ -117,16 +51,14 @@ client.on("messageCreate", (message) => {
       message.channel.send("ユーザー名を入力してください");
       return;
     }
-    const info = getContestInfo(username);
+    const info = AtCoderAPI.getContestInfo(username, contestInfo);
     if (!info) {
       message.channel.send(`${username}は登録されていませんでした`);
       return;
     }
-    // userの最新のレートを取得,contestResultの最後の要素が最新のレート
     const latestRating =
       info.contestResult[info.contestResult.length - 1].NewRating;
     message.channel.send(`${username}の最新のレートは${latestRating}です`);
-    console.log(latestRating);
   }
 });
 
