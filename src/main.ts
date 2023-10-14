c7332a7299d41c31278a3dcef1ef95f9f8849027
kasa021 <mkafa.k11@gmail.com>
2023-10-15 02:01:33 +0900

    refactor:コマンドの実装をhandle関数に書き換えた

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { GatewayIntentBits, Client, Partials } from "discord.js";
 import { AtCoderContestInfo } from "./interface";
 import * as AtCoderAPI from "./atcoderAPI";
+
 dotenv.config();
 
 const contestInfo: AtCoderContestInfo[] = [];
@@ -17,56 +18,70 @@ const client = new Client({
   partials: [Partials.Message, Partials.Channel],
 });
 
-// addコマンド: ユーザーのAtCoderの情報を追加する
+import { Message } from "discord.js";
+
+async function handleAddCommand(message: Message, username: string) {
+  if (!username) {
+    message.channel.send("ユーザー名を入力してください");
+    return;
+  }
+  const response = await AtCoderAPI.addContestInfo(username, contestInfo);
+  message.channel.send(response);
+}
+
+function handleDeleteCommand(message: Message, username: string) {
+  if (!username) {
+    message.channel.send("ユーザー名を入力してください");
+    return;
+  }
+  const response = AtCoderAPI.deleteContestInfo(username, contestInfo);
+  message.channel.send(response);
+}
+
+function handleShowCommand(message: Message, username: string) {
+  if (!username) {
+    message.channel.send("ユーザー名を入力してください");
+    return;
+  }
+  const info = AtCoderAPI.getContestInfo(username, contestInfo);
+  if (!info) {
+    message.channel.send(`${username}は登録されていませんでした`);
+    return;
+  }
+  const latestRating =
+    info.contestResult[info.contestResult.length - 1].NewRating;
+  message.channel.send(`${username}の最新のレートは${latestRating}です`);
+}
+
+function handleListCommand(message: Message) {
+  if (contestInfo.length === 0) {
+    message.channel.send("ユーザーが登録されていませんでした");
+    return;
+  }
+  const list = contestInfo.map((item) => item.username).join("\n");
+  message.channel.send(list);
+}
+
 client.on("messageCreate", async (message) => {
-  if (message.content.startsWith("!add")) {
-    const username = message.content.split(" ")[1];
-    if (!username) {
-      message.channel.send("ユーザー名を入力してください");
-      return;
-    }
-    const response = await AtCoderAPI.addContestInfo(username, contestInfo);
-    message.channel.send(response);
-  }
-});
+  const content = message.content;
+  const command = content.split(" ")[0];
+  const username = content.split(" ")[1];
 
-// deleteコマンド: ユーザーのAtCoderの情報を削除する
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("!delete")) {
-    const username = message.content.split(" ")[1];
-    if (!username) {
-      message.channel.send("ユーザー名を入力してください");
-      return;
-    }
-    const response = AtCoderAPI.deleteContestInfo(username, contestInfo);
-    message.channel.send(response);
-  }
-});
-
-// showコマンド: ユーザーのAtCoderの情報を表示する
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("!show")) {
-    const username = message.content.split(" ")[1];
-    if (!username) {
-      message.channel.send("ユーザー名を入力してください");
-      return;
-    }
-    const info = AtCoderAPI.getContestInfo(username, contestInfo);
-    if (!info) {
-      message.channel.send(`${username}は登録されていませんでした`);
-      return;
-    }
-    const latestRating =
-      info.contestResult[info.contestResult.length - 1].NewRating;
-    message.channel.send(`${username}の最新のレートは${latestRating}です`);
-  }
-});
-
-// listコマンド: 登録しているユーザーの一覧を表示する
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("!list")) {
-    const list = contestInfo.map((item) => item.username).join("\n");
-    message.channel.send(list);
+  switch (command) {
+    case "!add":
+      await handleAddCommand(message, username);
+      break;
+    case "!delete":
+      handleDeleteCommand(message, username);
+      break;
+    case "!show":
+      handleShowCommand(message, username);
+      break;
+    case "!list":
+      handleListCommand(message);
+      break;
+    default:
+      break;
   }
 });
 
